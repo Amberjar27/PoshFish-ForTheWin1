ea6c4ada23a133c758a34baf37a38f4e1cf63831
n-okonek <41804767+n-okonek@users.noreply.github.com>
2023-01-22 15:51:42 -0600

    add banner to issue file

--- youshallnotpass.sh
+++ youshallnotpass.sh
@@ -8,8 +8,9 @@
 # Should allow for uninterupted insallation of SSH
 sudo apt-get install ssh -y --allow-unauthenticated
 
-# Make a back-up of the sshd_config file
+# Make a back-up of the original files
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
+sudo cp /etc/issue /etc/issue.orig
 
 # Create banner text
 echo "************************* WARNING! *************************" >> /etc/ssh/metro_banner
@@ -20,6 +21,16 @@ echo "*    consent to monitoring, retrieval, and disclosure of   *" >> /etc/ssh/
 echo "*  any activity. Any unauthorized activity will be subject *" >> /etc/ssh/metro_banner
 echo "*        to requests to stop, pretty pretty please.        *" >> /etc/ssh/metro_banner
 echo "************************************************************" >> /etc/ssh/metro_banner
+
+# Create banner text
+echo "************************* WARNING! *************************" >> /etc/issue
+echo "*  This computer system is intended for * authorized users *" >> /etc/issue
+echo "*  only. You should have no expectation of privacy in your *" >> /etc/issue
+echo "*     use of this system. Use of this system constitutes   *" >> /etc/issue
+echo "*    consent to monitoring, retrieval, and disclosure of   *" >> /etc/issue
+echo "*  any activity. Any unauthorized activity will be subject *" >> /etc/issue
+echo "*        to requests to stop, pretty pretty please.        *" >> /etc/issue
+echo "************************************************************" >> /etc/issue
 
 # edit config
 sudo sed -i 's,#Banner none,Banner /etc/ssh/metro_banner,' /etc/ssh/sshd_config
