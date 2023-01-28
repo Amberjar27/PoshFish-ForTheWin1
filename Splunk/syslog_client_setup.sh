67bb943e41a022405ced35c85f5222b23d63daae
Liam Powell <Powell.liam.p@gmail.com>
2023-01-27 21:48:36 -0600

    Create syslog_client_setup.sh

---
+++ Splunk/syslog_client_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Splunk IP is $1"
+echo "Writing edits to /etc/rsyslog.conf"
+echo "*.* @@$1:601" >> /etc/rsyslog.conf
+echo "*.* @@$1:9997" >> /etc/rsyslog.conf
+echo "Confirming edits"
+tail -c 2 /etc/rsyslog.conf
+echo "Fixing SELinux"
+semanage port -a -t syslogd_port_t -p tcp 9997
+semanage port -a -t syslogd_port_t -p tcp 601
+echo "Restarting syslog service"
+systemctl restart rsyslog || service rsyslog restart
+echo "Sending test message"
+hostname=$(hostname)
+logger 'test message from $hostname'
+echo "Syslog forwarding is set up... Probably..."
