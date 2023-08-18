1383ba3f1ea17abc23ef42f8daf5eadb1bdacb62
K7-Avenger <48218837+K7-Avenger@users.noreply.github.com>
2023-08-17 21:31:24 -0500

    Update firewall.sh
    Added functionality to block reverse shells on unused ports

--- firewall.sh
+++ firewall.sh
@@ -21,6 +21,7 @@ RESET='\033[0m'
 # Drops attempted connections on ports not already explicityly defined as ACCEPT
 dropAll(){
   iptables -A INPUT -j DROP
+  iptables -A OUTPUT -j DROP #needs testing, but needed to prevent reverse shells on unused ports.
 }
 
 logFirewallEvents(){
