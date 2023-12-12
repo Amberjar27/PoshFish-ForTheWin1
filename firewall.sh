a4a3d1c18d0f25778216f0c735f3d668c7b415cc
boehkarl <146597696+boehkarl@users.noreply.github.com>
2023-12-11 18:28:02 -0600

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -130,19 +130,42 @@ allowSysLog(){
 
 setSplunk(){
   flushFirewall  #Flush all the bad rules
-  
-  #HTTP and HTTPS
-  iptables -A INPUT -p tcp --dport 80 -j ACCEPT   #Defining an interface is typically not required, but if you only want access from your machine, the loopback adapter may be more appropriate.
+  # Drop all input and forward
+  iptables -P INPUT DROP
+  iptables -P FORWARD DROP
+
+  # ensure loopback is good 
+  iptables -A INPUT -i lo -j ACCEPT
+  iptables -A OUTPUT -o lo -j ACCEPT
+
+  # HTTP & HTTPS rules 
+  iptables -A INPUT -p tcp --sport 80 -j ACCEPT
   iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
-  iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+  iptables -A INPUT -p tcp --sport 443 -j ACCEPT
   iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
-  #Splunk Web UI
+
+  # Splunk WebGUI rules 
   iptables -A INPUT -p tcp --dport 8000 -j ACCEPT
-  iptables -A OUTPUT -p tcp --dport 8000 -j ACCEPT
-  #Splunk Management Port
+  iptables -A OUTPUT -p tcp --sport 8000 -j ACCEPT
+
+  # Splunk Management Port
   iptables -A INPUT -p tcp --dport 8089 -j ACCEPT
-  allowSysLog   # Opens the required ports for syslogs to be forwarded to datalake
-  dropAll
+
+  # Syslog traffic
+  iptables -A INPUT -p tcp --dport 9997 -j ACCEPT
+  iptables -A INPUT -p tcp --dport 9998 -j ACCEPT
+  iptables -A INPUT -p tcp --dport 601 -j ACCEPT
+  iptables -A INPUT -p udp --dport 514 -j ACCEPT
+
+  # Kill SSH traffic
+  iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j DROP
+  iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j DROP
+
+  # Allow established connections
+  iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT
+
+  #allowSysLog   # Opens the required ports for syslogs to be forwarded to datalake
+  #dropAll
   showFirewall
 }
 
