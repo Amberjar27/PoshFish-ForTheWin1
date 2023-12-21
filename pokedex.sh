f43b3e5ad9906342fc2d35ca23d1bd80ae770d90
K7-Avenger <48218837+K7-Avenger@users.noreply.github.com>
2023-12-20 20:31:49 -0600

    Update pokedex.sh
    Fixed ICMP rules

--- pokedex.sh
+++ pokedex.sh
@@ -93,6 +93,8 @@ allowClientSysLog(){
 
 allowICMP(){
   iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+  iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
+  iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
   iptables -A OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT
 }
 
