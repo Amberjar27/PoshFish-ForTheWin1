6638399d3783a18688551486255b88ee56c4046e
boehkarl <146597696+boehkarl@users.noreply.github.com>
2024-01-19 15:49:03 -0600

    Update porygon.sh

--- porygon.sh
+++ porygon.sh
@@ -54,14 +54,13 @@ seven(){
   printf "Setting target to DROP"
   firewall-cmd --zone=public --set-target=DROP --permanent
 
-  sudo firewall-cmd --zone=public --add-interface=lo
+  firewall-cmd --zone=public --add-interface=lo
   
   firewall-cmd --zone=public --add-service=http
   firewall-cmd --zone=public --add-service=https
   firewall-cmd --zone=public --add-port=53/udp
   firewall-cmd --zone=public --add-port=123/udp
   
-  printf "Splunk installed\nBuilding firewall rules\n"
   #  Serve Splunk GUI over 8000
   firewall-cmd --zone=public --add-port=8000/tcp
   # Splunk indexer
