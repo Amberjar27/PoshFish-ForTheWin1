aad3a9e515476a90e2a77dca82d75d88398795c0
tieniscoding <tnpwn@proton.me>
2025-01-18 13:35:30 -0600

    Update syslog_client_setup.sh

--- Splunk/syslog_client_setup.sh
+++ Splunk/syslog_client_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Splunk IP is $1"
 echo "Writing edits to /etc/rsyslog.conf"
-echo "*.* @@$1:601" >> /etc/rsyslog.conf
+echo "*.* @$1:1514" >> /etc/rsyslog.conf
+echo "*.* @@$1:1514" >> /etc/rsyslog.conf
 echo "*.* @@$1:9997" >> /etc/rsyslog.conf
 echo "Confirming edits"
 tail -c 50 /etc/rsyslog.conf
