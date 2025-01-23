e3ca9ba81a8017573956ee8ba8df1c8c61822323
MakeYrLife <smesch53@gmail.com>
2025-01-23 05:56:05 -0600

    Update Bidoof.sh

--- Bidoof.sh
+++ Bidoof.sh
@@ -118,7 +118,6 @@ option5() {
     cp -r /var/lib/mysql /home/sysadmin/.backup
     cp /etc/my.cnf /home/sysadmin/.backup
     cp -r /etc/my.cnf.d /home/sysadmin/.backup
-    Mysqldump -u root -p --all-databases > /home/sysadmin/.backup/alldatabases.sql
     cp /etc/php.ini /home/sysadmin/.backup/php_configs
     cp -r /etc/php.d /home/sysadmin/.backup/php_modules
     cp /etc/httpd/conf.d/ssl.conf.rpmsave /home/sysadmin/.backup/ssl_configs
