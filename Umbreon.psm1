17ade09ed313e78a568335ffcf42c57393d265d8
Silas Powers <silas.powers@my.metrostate.edu>
2025-01-23 16:18:36 -0600

    Update Umbreon.psm1

--- Umbreon.psm1
+++ Umbreon.psm1
@@ -412,8 +412,19 @@ Write-Host "Welcome banner has been successfully set."
 }
 
 function splunkers {
-#For Tein
-wget -O splunkforwarder-9.4.0-6b4ebe426ca6-windows-x64.msi "https://download.splunk.com/products/universalforwarder/releases/9.4.0/windows/splunkforwarder-9.4.0-6b4ebe426ca6-windows-x64.msi"
+#For Tien logging
+param($ip)
+invoke-webrequest -URI "https://download.splunk.com/products/universalforwarder/releases/9.0.3/windows/splunkforwarder-9.0.3-dd0128b1f8cd-x64-release.msi" -outfile splunkuniversalforwarder_x86.msi
+$ipr = "$ip`:9998"
+$args=@"
+/i splunkuniversalforwarder_x86.msi RECEIVING_INDEXER={0} AGREETOLICENSE=Yes SPLUNKUSERNAME=SplunkAdmin GENRANDOMPASSWORD=1 WINEVENTLOG_SEC_ENABLE=1 WINEVENTLOG_SYS_ENABLE=1 WINEVENTLOG_APP_ENABLE=1 WINEVENTLOG_FWD_ENABLE=1 WINEVENTLOG_SET_ENABLE=1 /quiet
+"@ -f $ipr
+write-host "Splunk IP: $ipr"
+write-host "Executing with arguments: $args"
+Start-Process msiexec.exe -Wait -ArgumentList $args
+$pass=Get-content $env:TEMP\splunk.log | select-string -Pattern "Password="
+write-host "Password for splunk user:" $pass
+Remove-Item $env:TEMP\splunk.log
 wget -O sysmonconfig-export.xml "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/refs/heads/master/sysmonconfig-export.xml"
 Invoke-WebRequest -Uri https://download.sysinternals.com/files/Sysmon.zip -OutFile "$env:USERPROFILE\Sysmon.zip"
 Expand-Archive -Path "$env:USERPROFILE\Sysmon.zip" -DestinationPath "$env:USERPROFILE\Sysmon"
